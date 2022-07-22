8fafcac306b1f19ea82e2376d34bcbfec13bd1b2
Pedrovg213 <92651971+Pedrovg213@users.noreply.github.com>
2022-07-22 15:35:58 -0300

    Generics
    - Using a class to find the highest value within a list, but without using generics yet.

---
+++ Prototype01/Prototype01/PrintService.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Prototype01 {
+   internal class PrintService<T> {
+
+      private T[ ] Values = new T[10];
+      private int Count = 0;
+
+
+      public void AddValue( T _value ) {
+
+         if ( Count == 10 )
+            throw new InvalidOperationException( "PrintService is full." );
+
+         Values[ Count ] = _value;
+         Count++;
+
+      }
+      public T First( ) {
+
+         if ( Count == 0 )
+            throw new InvalidOperationException( "PrintService is empty." );
+
+         return ( Values[ 0 ] );
+
+      }
+      public void Print( ) {
+
+         Console.Write( "[" );
+         for ( int i = 0; i < Count - 1; i++ )
+            Console.Write( Values[ i ] + ", " );
+         if ( Count > 0 )
+            Console.Write( Values[ Count - 1 ] );
+         Console.WriteLine( "]" );
+
+      }
+   }
+}
--- Prototype01/Prototype01/Program.cs
+++ Prototype01/Prototype01/Program.cs
@@ -1,8 +1,23 @@
 ﻿namespace Prototype01 {
    internal class Program {
       static void Main( string[ ] args ) {
-         
 
+         PrintService<int> service = new PrintService<int>();
+
+         //inputs
+         Console.Write( "How many values? " );
+         int valuesRage = int.Parse(Console.ReadLine());
+
+         for ( int i = 0; i < valuesRage; i++ ) {
+
+            int value = int.Parse(Console.ReadLine());
+
+            service.AddValue( value );
+
+         }
+
+         service.Print();
+         Console.WriteLine( "First: " + service.First() );
 
       }
    }
