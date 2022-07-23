773be719b873992a015134f0ee9d65a4a750f053
Pedrovg213 <92651971+Pedrovg213@users.noreply.github.com>
2022-07-23 00:30:27 -0300

    GetHashCode and Equals
    - Learning about and using GetHashCode() and Equal() overwriting.

---
+++ Prototype03/Prototype03/Entities/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Prototype03.Entities {
+   internal class Client {
+
+      public string Name {
+         get; set;
+      }
+      public string Email {
+         get; set;
+      }
+
+
+      public Client( string _name , string _email ) {
+      
+         Name = _name;
+         Email = _email;
+
+      }
+
+
+      public override bool Equals( object? obj ) {
+
+         return (( obj as Client ).Email.Equals( Email ));
+
+      }
+      public override int GetHashCode( ) {
+
+         return ( Email.GetHashCode() );
+
+      }
+   }
+}
---
+++ Prototype03/Prototype03/Program.cs
@@ -0,0 +1,17 @@
+﻿using Prototype03.Entities;
+
+namespace Prototype03 {
+   internal class Program {
+      static void Main( string[ ] args ) {
+
+         Client client01 = new Client("Maria", "maria@gmail.com");
+         Client client02 = new Client("Alex", "maria@gmail.com");
+
+         Console.WriteLine( client01.Equals( client02 ) );
+         Console.WriteLine( client01 == client02 );
+         Console.WriteLine( client01.GetHashCode() );
+         Console.WriteLine( client02.GetHashCode() );
+
+      }
+   }
+}
