bfaa6e92866161942b43f82cdca616057acf014d
Pedrovg213 <92651971+Pedrovg213@users.noreply.github.com>
2022-07-23 00:06:09 -0300

    Generics Restriction
    - Using Generics Restriction to prevent entry of a type that can not be comparable.

---
+++ Prototype02/Prototype02/Entities/Products.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Prototype02.Entities {
+   internal class Products : IComparable {
+
+      public string Name {
+         get; set;
+      }
+      public double Price {
+         get; set;
+      }
+
+
+      public Products( string _name , double _price ) {
+
+         Name = _name;
+         Price = _price;
+
+      }
+
+
+
+      public int CompareTo( object? obj ) {
+
+         if ( !( obj is Products ) )
+            throw new ArgumentException( "Comparing erro: argument is not a product" );
+
+         Products other = obj as Products;
+         return Price.CompareTo( other.Price );
+
+      }
+      public override string ToString( ) {
+
+         return ( Name + ", " + Price.ToString( "F2" , CultureInfo.InvariantCulture ) );
+
+      }
+   }
+}
---
+++ Prototype02/Prototype02/Services/CalculationService.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Prototype02.Services {
+   internal class CalculationService {
+
+      public T Max<T>( List<T> _list ) where T : IComparable {
+
+         if ( _list.Count == 0 )
+            throw new ArgumentException( "The list can not be empty." );
+
+         T max = _list[0];
+
+         for ( int i = 1; i < _list.Count; i++ )
+            if ( _list[ i ].CompareTo( max ) > 0 )
+               max = _list[ i ];
+
+         return ( max );
+      }
+   }
+}
