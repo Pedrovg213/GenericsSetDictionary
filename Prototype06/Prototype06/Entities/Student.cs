c146b6475d9660f2f90a868113fa47790938cc2a
Pedrovg213 <92651971+Pedrovg213@users.noreply.github.com>
2022-07-25 13:55:54 -0300

    Prototype06: finished
    - Using HashSet to return the total of students enrolled in three courses. Where the students may be enrolled in more than one course.

---
+++ Prototype06/Prototype06/Entities/Student.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Prototype06.Entities {
+   internal class Student {
+
+      public int Id {
+         get; set;
+      }
+
+
+      public Student( int _id ) {
+
+         Id = _id;
+
+      }
+
+
+      public override bool Equals( object? obj ) {
+
+         if ( !( obj is Student ) )
+            return ( false );
+
+         Student other = (Student)obj;
+
+         return ( Id.Equals( other.Id ) );
+
+      }
+      public override int GetHashCode( ) {
+
+         return ( Id.GetHashCode() );
+
+      }
+   }
+}
--- Prototype06/Prototype06/Program.cs
+++ Prototype06/Prototype06/Program.cs
@@ -1,7 +1,61 @@
-﻿namespace Prototype06 {
+﻿using Prototype06.Entities;
+
+namespace Prototype06 {
    internal class Program {
       static void Main( string[ ] args ) {
-         
+
+         // INPUTS
+         // Course A
+         Console.Write( "How many students for course A? " );
+
+         int studentsNum = int.Parse(Console.ReadLine());
+         HashSet<Student> studentsA = new HashSet<Student>();
+
+         for ( int i = 0; i < studentsNum; i++ ) {
+
+            Console.Write( "Enter student ID: " );
+            studentsA.Add( new Student( int.Parse( Console.ReadLine() ) ) );
+
+         }
+         Console.WriteLine();
+
+         // Course B
+         Console.Write( "How many students for course B? " );
+
+         studentsNum = int.Parse( Console.ReadLine() );
+         HashSet<Student> studentsB = new HashSet<Student>();
+
+         for ( int i = 0; i < studentsNum; i++ ) {
+
+            Console.Write( "Enter student ID: " );
+            studentsB.Add( new Student( int.Parse( Console.ReadLine() ) ) );
+
+         }
+         Console.WriteLine();
+
+         // Course C
+         Console.Write( "How many students for course C? " );
+
+         studentsNum = int.Parse( Console.ReadLine() );
+         HashSet<Student> studentsC = new HashSet<Student>();
+
+         for ( int i = 0; i < studentsNum; i++ ) {
+
+            Console.Write( "Enter student ID: " );
+            studentsC.Add( new Student( int.Parse( Console.ReadLine() ) ) );
+
+         }
+         Console.WriteLine();
+
+         // PROCESS
+         HashSet<Student> totalStudents = new HashSet<Student>();
+         totalStudents.UnionWith( studentsA );
+         totalStudents.UnionWith( studentsB );
+         totalStudents.UnionWith( studentsC );
+
+         // OUTPUT
+         Console.Write( "TOTAL STUDENTS: " + totalStudents.Count );
+
       }
    }
 }
